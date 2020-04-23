0596486f8610a9b297041ae88fca9fa67fcac2d8
Joaquin Lopez <mrgus@disco-zombie.net>
2020-04-23 12:18:41 -0700

    Output resource group name

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "kubeconfig" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
+
+# TODO: Make it possible to input so we don't have to round-trip this
+output "resource_group_name" {
+  value = azurerm_resource_group.k8s.name
+}
