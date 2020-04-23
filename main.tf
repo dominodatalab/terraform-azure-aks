353c2b2cae436f54bedd51c1071657a741f9b060
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2020-04-23 14:19:31 -0600

    Add Circle URL

--- main.tf
+++ main.tf
@@ -143,9 +143,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     service_cidr       = "100.97.0.0/16"
   }
 
-  tags = {
-    Environment = "Development"
-  }
+  tags = merge({Environment = "Development"}, var.aks_tags)
 
 }
 
