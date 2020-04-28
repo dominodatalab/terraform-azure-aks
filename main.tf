7943357d75035fb40b4c27a98a2511b1445182af
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2020-04-28 09:45:28 -0700

    remove subscription ID

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location                   = azurerm_resource_group.k8s.location
   resource_group_name        = azurerm_resource_group.k8s.name
   dns_prefix                 = local.cluster_name
-  private_link_enabled       = false
+  private_cluster_enabled    = false
 
   api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
 
