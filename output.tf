58f965ed88df0401344977212bba22d81ce5d231
Joaquin Lopez <mrgus@disco-zombie.net>
2020-04-28 12:52:22 -0700

    Remove outdated comment

--- output.tf
+++ output.tf
@@ -2,7 +2,6 @@ output "kubeconfig" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
 
-# TODO: Make it possible to input so we don't have to round-trip this
 output "resource_group" {
   value = local.resource_group
 }
