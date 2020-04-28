d6f0ca31154a11ee9550725b7c04cc30fba06d98
Joaquin Lopez <mrgus@disco-zombie.net>
2020-04-28 16:09:30 -0700

    Preserve alphabetical order

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "containers" {
+  value = azurerm_storage_container.domino_containers
+}
+
 output "kubeconfig" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
@@ -8,8 +12,4 @@ output "resource_group" {
 
 output "storage_account" {
   value = azurerm_storage_account.domino
-}
-
-output "containers" {
-  value = azurerm_storage_container.domino_containers
 }
