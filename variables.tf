a99eb371c74a2d7fe9ec4e64ad57259fcbb2298e
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2021-05-05 19:07:04 -0700

    allow partial kubernetes versions

--- variables.tf
+++ variables.tf
@@ -147,5 +147,5 @@ variable "tags" {
 variable "kubernetes_version" {
   type        = string
   default     = null
-  description = "Optional Kubernetes version to provision"
+  description = "Optional Kubernetes version to provision. Allows partial input (e.g. 1.18) which is then chosen from azurerm_kubernetes_service_versions."
 }
