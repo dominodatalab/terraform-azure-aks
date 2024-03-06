e91e2d39aed21f67eb3fc0925fed459ad955b897
Po Cheung <po.cheung@dominodatalab.com>
2024-03-06 12:06:16 -0800

    Update federated identities

--- modules/flyte/identities.tf
+++ modules/flyte/identities.tf
@@ -17,7 +17,7 @@ resource "azurerm_federated_identity_credential" "flyteadmin" {
   resource_group_name = var.azurerm_resource_group_name
   audience            = ["api://AzureADTokenExchange"]
   issuer              = var.azurerm_kubernetes_cluster_oidc_issuer_url
-  parent_id           = azurerm_user_assigned_identity.flyte_controlplane.id
+  parent_id           = azurerm_user_assigned_identity.flyte_dataplane.id
   subject             = "system:serviceaccount:${var.namespaces.platform}:${var.serviceaccount_names.flyteadmin}"
 }
 
@@ -35,6 +35,6 @@ resource "azurerm_federated_identity_credential" "datacatalog" {
   resource_group_name = var.azurerm_resource_group_name
   audience            = ["api://AzureADTokenExchange"]
   issuer              = var.azurerm_kubernetes_cluster_oidc_issuer_url
-  parent_id           = azurerm_user_assigned_identity.flyte_dataplane.id
+  parent_id           = azurerm_user_assigned_identity.flyte_controlplane.id
   subject             = "system:serviceaccount:${var.namespaces.platform}:${var.serviceaccount_names.datacatalog}"
 }
