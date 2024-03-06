1cf42454eed8c86075e493fa0476f00250f9105f
Po Cheung <po.cheung@dominodatalab.com>
2024-03-06 11:21:09 -0800

    Fix group membership

--- modules/flyte/groups.tf
+++ modules/flyte/groups.tf
@@ -3,6 +3,7 @@ resource "azuread_group" "flyte_metadata_access" {
   security_enabled = true
   members = [
     azurerm_user_assigned_identity.flyte_controlplane.id,
+    azurerm_user_assigned_identity.flyte_dataplane.id,
   ]
 }
 
@@ -10,7 +11,6 @@ resource "azuread_group" "flyte_data_access" {
   display_name     = "Flyte data access"
   security_enabled = true
   members = [
-    azurerm_user_assigned_identity.flyte_controlplane.id,
     azurerm_user_assigned_identity.flyte_dataplane.id,
   ]
 }
