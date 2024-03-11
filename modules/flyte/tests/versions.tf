77a708a2c69a6f8bf69bec1bd6316e41595db019
Po Cheung <po.cheung@dominodatalab.com>
2024-03-11 00:38:18 -0700

    Add unit tests

---
+++ modules/flyte/tests/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.40"
+    }
+  }
+}
