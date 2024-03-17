6fcf8d0784b95e581289ed7be17ab894698806e5
Po Cheung <po.cheung@dominodatalab.com>
2024-03-17 15:27:53 -0700

    Update service account to workload identity mapping

--- modules/flyte/identities.tf
+++ modules/flyte/identities.tf
@@ -1,14 +1,15 @@
 locals {
+  identities = ["flyte_controlplane", "flyte_dataplane"]
   # Kubernetes service account to user-assigned managed identity mapping
   mapping = {
-    flyteadmin     = "flyte_dataplane"
+    flyteadmin     = "flyte_controlplane"
     flytepropeller = "flyte_controlplane"
     datacatalog    = "flyte_controlplane"
   }
 }
 
 resource "azurerm_user_assigned_identity" "this" {
-  for_each            = toset(values(local.mapping))
+  for_each            = toset(local.identities)
   name                = each.key
   location            = var.azurerm_resource_group_location
   resource_group_name = var.azurerm_resource_group_name
--- modules/flyte/tests/outputs.tftest.hcl
+++ modules/flyte/tests/outputs.tftest.hcl
@@ -5,12 +5,12 @@ run "test_outputs" {
   command = plan
 
   assert {
-    condition     = output.flyte_metadata_container_name == azurerm_storage_container.flyte_metadata.name
+    condition     = output.metadata_container_name == azurerm_storage_container.flyte_metadata.name
     error_message = "Incorrect Flyte metadata container name output"
   }
 
   assert {
-    condition     = output.flyte_data_container_name == azurerm_storage_container.flyte_data.name
+    condition     = output.data_container_name == azurerm_storage_container.flyte_data.name
     error_message = "Incorrect Flyte data container name output"
   }
 }
