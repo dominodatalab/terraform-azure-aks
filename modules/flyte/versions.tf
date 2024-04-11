77de1bdc47fe552bfc8f16be1f64235b9c9ce1c9
Po Cheung <po@dominodatalab.com>
2024-04-10 23:54:26 -0700

    DOM-54279 Remove AD groups (#53)

--- modules/flyte/versions.tf
+++ modules/flyte/versions.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.0"
-    }
-
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.45"
